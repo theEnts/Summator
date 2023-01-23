889e8533fff3c3c69fce32e042e60be1ffad7bf4
vladislavzagorov <vladislavzagorov@gmail.com>
2023-01-23 23:23:05 +0200

    added new test

--- SummatorTests/TestSetSummator.cs
+++ SummatorTests/TestSetSummator.cs
@@ -19,5 +19,16 @@ namespace SummatorTests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void Test_SumTwoNegativeNumbers()
+        {
+            var nums = new[] { -1, -2 };
+            var actual = Summator_Class.Sum(nums);
+
+            var expected = -3;
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
