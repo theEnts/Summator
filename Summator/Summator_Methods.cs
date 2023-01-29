1122afb22155f444cbcbb2f8f87b672f8b9e8ad3
vladislavzagorov <vladislavzagorov@gmail.com>
2023-01-29 12:09:42 +0200

    add_new_methods_and_test_cases

---
+++ Summator/Summator_Methods.cs
@@ -0,0 +1,65 @@
+﻿namespace Summator
+{
+    public static class Summator_Methods
+    {
+        public static long Sum(int[] arr)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        public static double Average(double[] arr)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum / arr.Length;
+        }
+
+        public static double Multiplication(int[] arr)
+        {
+
+            int x = arr[0];
+            int y = arr[1];
+
+            int quotient = 0;
+
+            for (int i = 0; i < y; i++)
+            {
+                quotient += x;
+            }
+            return quotient;
+        }
+
+        public static double PowerOfNumbers(double[] arr)
+        {
+            double result = Math.Pow(arr[0], arr[1]);
+            return result;
+        }
+
+        public static int Divide(int[] arr)
+        {
+            int x = arr[0]; 
+            int y = arr[1];
+
+            int quotient = 0;
+
+            while (x - y >=0)
+            {
+                x -= y;
+                quotient += 1;
+            }
+            return quotient;
+
+
+        }
+    }
+}
--- SummatorTests/TestSetSummator.cs
+++ SummatorTests/TestSetSummator.cs
@@ -1,19 +1,16 @@
-﻿using Summator;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework.Constraints;
+using Summator;
+using System.Collections;
 
 namespace SummatorTests
 {
     public class TestSetSummator
     {
         [Test]
-        public void Test_SumTwoPositiveNumbers()
+        public void Test_Sum_Two_Positive_Numbers()
         {
-            var nums = new[] { 1, 2 };
-            var actual = Summator_Class.Sum(nums);
+            var nums = new int[] { 1, 2 };
+            var actual = Summator_Methods.Sum(nums);
 
             var expected = 3;
 
@@ -21,12 +18,101 @@ namespace SummatorTests
         }
 
         [Test]
-        public void Test_SumTwoNegativeNumbers()
+        public void Test_Sum_Two_Negative_Numbers()
         {
-            var nums = new[] { -1, -2 };
-            var actual = Summator_Class.Sum(nums);
+            var nums = new int[] { -1, -2 };
+            var actual = Summator_Methods.Sum(nums);
 
             var expected = -3;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Sum_One_Positive_Number()
+        {
+            var nums = new int[] { 2 };
+            var actual = Summator_Methods.Sum(nums);
+
+            var expected = 2;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Sum_Zero_Number()
+        {
+            var nums = new int[] { };
+            var actual = Summator_Methods.Sum(nums);
+
+            var expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Sum_Big_Number()
+        {
+            var nums = new int[] { 2000000000, 2000000000, 2000000000};
+            var actual = Summator_Methods.Sum(nums);
+
+            var expected = 6000000000;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Average_Positive_Number()
+        {
+            var nums = new double[] { 6, 9, 8 };
+            var actual = Summator_Methods.Average(nums);
+
+            var expected = 7.67;
+
+            actual = Math.Round(actual, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Average_With_Negative_Number()
+        {
+            var nums = new double[] { 6, 9, 8, -7.9 };
+            var actual = Summator_Methods.Average(nums);
+
+            var expected = 3.78;
+
+            actual = Math.Round(actual, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Test_Multiply_With_Two_Numbers()
+        {
+            var nums = new int[] { 8, 4 };
+            int actual = (int)Summator_Methods.Multiplication(nums);
+            var expected = 32;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void Test_Power_Of_Two_Numbers()
+        {
+            var nums = new double[] { 5, 2 };
+            double actual = Summator_Methods.PowerOfNumbers(nums);
+            double expected = 25;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_Division_Of_Two_Numbers()
+        {
+            var nums = new int[] { 15, 5 };
+            double actual = Summator_Methods.Divide(nums);  
+            double expected = 3;
 
             Assert.AreEqual(expected, actual);
         }
